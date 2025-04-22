a74f51e3f6982efcfcc1cfd218658045410e668c
priyanka <priyanka@leapfunder.nl>
2025-04-22 19:13:03 +0530

    code refactore

--- lib/string_calculator.rb
+++ lib/string_calculator.rb
@@ -2,7 +2,10 @@ class StringCalculator
     def self.add(number)
         return 0 if number.empty?
         delimeters = /,|\n/
-        delimeters = number.split("//")[1][0] if number.include?("//")
+        if number.start_with?("//")
+            delimeters, number = number.split("\n") 
+            delimeters = delimeters[2..]
+        end
         int_numb = number.split(delimeters).map(&:to_i)
         negative_numb = int_numb.select {|n| n.negative? }
         raise RuntimeError, "negative numbers not allowed" unless  negative_numb.empty?
