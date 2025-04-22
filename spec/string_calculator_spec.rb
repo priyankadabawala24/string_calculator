a3c320bedea2cc608b25c0f5e10fde9baa38e249
priyanka <priyanka@leapfunder.nl>
2025-04-22 18:06:51 +0530

    test: added case for return number itself is single number given

--- spec/string_calculator_spec.rb
+++ spec/string_calculator_spec.rb
@@ -4,4 +4,8 @@ RSpec.describe StringCalculator do
     it "return 0 if the string is empty" do 
         expect(StringCalculator.add("")).to eq(0)
     end
+
+    it "return number itself if single number" do 
+        expect(StringCalculator.call(1)).to eq(1)
+    end 
 end
